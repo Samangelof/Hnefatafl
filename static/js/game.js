fb31a351f6d96a71fa9093d946c461f126ea7cc0
Samangelof <ksa1sano@mail.ru>
2024-05-06 23:09:18 +0300

    code sharing js

---
+++ static/js/game.js
@@ -0,0 +1,146 @@
+// функция снимает выделение со всех клеток на доске
+function clearSelection() {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.classList.remove('selected');
+    });
+}
+
+function sendMove(from_row, from_col, to_row, to_col, player_type) {
+    fetch('/move_piece', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            from_row: from_row,
+            from_col: from_col,
+            to_row: to_row,
+            to_col: to_col,
+            player_type: player_type
+        }),
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Invalid move');
+        }
+        return response.text();
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// изначально ни одна клетка не выбрана
+let selectedCell = null;
+let player_type = confirm('Выберите сторону: attacker или defender. Нажмите "OK" для attacker, "Отмена" для defender.');
+
+if (player_type) {
+    console.log('Вы выбрали сторону attacker');
+    player_type = 'attacker';
+} else {
+    console.log('Вы выбрали сторону defender');
+    player_type = 'defender';
+}
+
+
+// const player_type = 'attacker';
+
+// const player_type = prompt('Выберите сторону: attacker или defender');
+// if (player_type !== 'attacker' && player_type !== 'defender') {
+//     alert('Неверный выбор стороны. Пожалуйста, выберите attacker или defender.');
+//     // можно добавить код для повторного запроса стороны или прерывания игры
+// }
+
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('click', () => {
+        // определение типа фигуры на клетке
+        const pieceType = cell.classList.contains('attacker') ? 'attacker' : (cell.classList.contains('defender') ? 'defender' : 'empty');
+        if ((player_type === 'attacker' && pieceType === 'defender') || (player_type === 'defender' && pieceType === 'attacker')) {
+            console.log('Игроку запрещено брать фигуры соперника');
+            return;
+        }
+
+
+        if (cell.classList.contains('selected')) {
+            // eсли клетка уже выбрана, сбрасываем выделение
+            cell.classList.remove('selected');
+            selectedCell = null;
+            return;
+        }
+        if (selectedCell) {
+            // если уже есть выбранная клетка, сбрасываем ее выделение
+            selectedCell.classList.remove('selected');
+        }
+
+        if (selectedCell && cell.classList.contains('empty')) {
+            const from_row = parseInt(selectedCell.getAttribute('data-row'));
+            const from_col = parseInt(selectedCell.getAttribute('data-col'));
+            const to_row = parseInt(cell.getAttribute('data-row'));
+            const to_col = parseInt(cell.getAttribute('data-col'));
+            // 
+
+
+            if (from_row !== to_row && from_col !== to_col) {
+                console.log('Неверный ход: необходимо перемещаться по горизонтали или вертикали.');
+                return;
+            }
+            fetch('/move_piece',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        from_row: from_row,
+                        from_col: from_col,
+                        to_row: to_row,
+                        to_col: to_col,
+                        player_type: player_type  // здесь нужно учитывать тип игрока
+                    }),
+                }
+            ).then(response => {
+                if (!response.ok) {
+                    throw new Error('Invalid move');
+                }
+                return response.text();
+            }).then(data => {
+                console.log('[DATA]', data);
+                // Обновление игрового состояния на фронте
+                const piece = selectedCell.innerHTML;
+                selectedCell.innerHTML = '';
+                cell.innerHTML = piece;
+                selectedCell.classList.remove('selected');
+                cell.classList.add('selected');
+
+                if (pieceType === 'attacker') {
+                    // удаляются классы defender и attacker
+                    selectedCell.classList.remove('defender')
+                    selectedCell.classList.remove('attacker')
+
+                    // пометить начальную клетку как пустую
+                    selectedCell.classList.add('empty');
+                }
+                else {
+                    selectedCell.classList.remove('defender')
+                    selectedCell.classList.remove('attacker')
+
+                    selectedCell.classList.add('empty');
+
+                }
+                // selectedCell.classList.add('empty');
+                selectedCell = null;
+
+                // Снять выделения со всех клеток
+                clearSelection();
+            })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        } else {
+            // если нет выбранной клетки, выбираем текущую, если на ней есть фигура
+            if (cell.innerHTML.trim()) {
+                cell.classList.add('selected');
+                selectedCell = cell;
+            }
+        }
+    });
+});
+
---
+++ static/js/socket.js
@@ -0,0 +1,69 @@
+let socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('user_connected', function (data) {
+    let nickname = data.nickname;
+    console.log(`${nickname} вошел в игру`);
+    let playerList = document.getElementById('player-list');
+    let playerItem = document.createElement('li');
+    playerItem.textContent = nickname;
+    playerList.appendChild(playerItem);
+});
+
+// если не установить задержку, 
+// веб сокеты будут быстрее отрабатывать чем обновление доски
+// и у первого игрока фигура будет исчезать
+socket.on('move_piece', function (data) {
+    console.log('[MOVE PIECE]', data);
+    setTimeout(function () {
+        movePiece(data);
+        console.log('Board updated successfully.');
+    }, 100); 
+});
+
+
+socket.on('message', function (data) {
+    let item = document.createElement('li');
+    item.textContent = `${data.nickname}: ${data.message}`;
+    document.getElementById('messages').appendChild(item);
+});
+
+function movePiece(data) {
+    const fromRow = data.from_row;
+    const fromCol = data.from_col;
+    const toRow = data.to_row;
+    const toCol = data.to_col;
+
+    const fromCell = document.querySelector(`.cell[data-row="${fromRow}"][data-col="${fromCol}"]`);
+    console.log('Откуда', fromCell);
+    const toCell = document.querySelector(`.cell[data-row="${toRow}"][data-col="${toCol}"]`);
+    console.log('Куда', toCell);
+
+    if (data.player_type === 'attacker') {
+        fromCell.innerHTML = ''
+        toCell.innerHTML = 'A';
+        toCell.classList.add('attacker')
+        toCell.classList.add('empty')
+        // toCell.classList.remove('attacker')
+    }
+    if (data.player_type === 'defender') {
+        fromCell.innerHTML = ''
+        toCell.innerHTML = 'D';
+        toCell.classList.add('defender')
+        toCell.classList.remove('empty')
+        // toCell.classList.remove('attacker')
+    }
+}
+
+function sendMessage() {
+    let message = document.getElementById('message').value;
+    socket.emit('message', message);
+    document.getElementById('message').value = '';
+}
